b7238e4dfad5286c9494702bb29b6a3dcb9bd1a1
“Juliana <132307762+JuSottaMoraes@users.noreply.github.com>
2024-09-03 17:41:11 -0300

    primeiro commit

---
+++ src/indice/Indice.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package indice;
+
+/**
+ *
+ * @author juliana
+ */
+public class Indice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int indice = 13;
+        int soma = 0;
+        int K = 0;
+        
+        while(K <indice) {
+            K = K + 1;
+            soma = soma + K;
+            
+        }
+        System.out.println(soma);
+                
+    }
+    
+}
